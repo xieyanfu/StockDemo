7517af07293068a4f4e72d6f0d24770ade136d9e
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2013-05-27 20:41:52 +0900

    README added

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-StockDemo
+StockDemo API reference
 =========
 
-Visualizing stock price
+## autocomplete
+**www.ai.cs.kobe-u.ac.jp/~fujikawa/softwares/StockDemo/api/autocomplete/?options<br>**
+銘柄名・銘柄コードに関する情報を取得するAPI
+
+### リクエスト
+
+| パラメータ | デフォルト | 説明 |
+|----------------------------|
+| q | NULL | 会社名の頭文字 |
+
+### レスポンス
+
+
+## getStockPrice
+**www.ai.cs.kobe-u.ac.jp/~fujikawa/softwares/StockDemo/api/getStockPrice/?options**<br>
+指定した銘柄の、指定した期間の株価を取得するAPI
+
+### リクエスト
+
+| パラメータ | デフォルト | 説明 |
+|----------------------------|
+| brand_code | 0101 | 銘柄コード（日経平均:0101, NTT data: 9613, ヤフー: 4689, 東京電力: 9501 etc.） |
+| from | 2008-01-01 | 取得開始する日付 |
+| to | 2008-01-08 | 取得終了する日付 |
--- js/charts.js
+++ js/charts.js
@@ -8,12 +8,12 @@ google.load('visualization', '1.0', {
 });
 
 function computeDate(year, month, day, addDays) {
-    var dt = new Date(year, month - 1, day);
-    var baseSec = dt.getTime();
-    var addSec = addDays * 86400000;//日数 * 1日のミリ秒数
-    var targetSec = baseSec + addSec;
-    dt.setTime(targetSec);
-    return dt.getFullYear() + '-' + String(Number(dt.getMonth()) + 1) + '-' + dt.getDate();
+	var dt = new Date(year, month - 1, day);
+	var baseSec = dt.getTime();
+	var addSec = addDays * 86400000;//日数 * 1日のミリ秒数
+	var targetSec = baseSec + addSec;
+	dt.setTime(targetSec);
+	return dt.getFullYear() + '-' + String(Number(dt.getMonth()) + 1) + '-' + dt.getDate();
 }
 
 function computeToDate(){
@@ -38,67 +38,123 @@ function computeToDate(){
 
 var UISliders = function () {
 	var interval = ['1 week', '1 month', '6 month', '1 year', '2 year','4 year']
-    return {
-        //main function to initiate the module
-        initSliders: function () {
-            $("#slider-range-max").slider({
-                range: "max",
-                min: 0,
-                max: 5,
-                value: 1,
-                slide: function (event, ui) {
-                    $("#slider-range-max-amount").text(interval[Number(ui.value)]);
-                }
-            });
+	return {
+		//main function to initiate the module
+		initSliders: function () {
+			$("#slider-range-max").slider({
+				range: "max",
+				min: 0,
+				max: 5,
+				value: 1,
+				slide: function (event, ui) {
+					$("#slider-range-max-amount").text(interval[Number(ui.value)]);
+				}
+			});
 
-            $("#slider-range-max-amount").text(interval[Number($("#slider-range-max").slider("value"))]);
+			$("#slider-range-max-amount").text(interval[Number($("#slider-range-max").slider("value"))]);
 
-        }
-    };
+		}
+	};
 
 }();
 
-
-function getStockPrice() {
-	console.log($("#input_date").attr("value"));
-	baseURL = "http://www.ai.cs.kobe-u.ac.jp/~fujikawa/softwares/StockDemo/api/getStockPrice?";
-	var data = {
-		brand_code : selected_brand_code,
-		from : $("#input_date").attr("value"),
-		to: computeToDate()
-	};
-	$.ajax({
-		type: "GET",
-		url: baseURL,
-		data: data,
-		dataType: 'jsonp',
-		success: function(response){
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', '日付');
-			data.addColumn('number', selected_brand_name);
-			array = [];
-			for(i=0; i<response.data.length; i++){
-				console.log(response.data[i].opening_price);
-				array.push([response.data[i].date, Number(response.data[i].opening_price)])
-
-			}
-			data.addRows(array);
-			console.log(data);
-			google.setOnLoadCallback(drawChart(data));
-		}
-	});
+function addQueryLog(query) {
+	$("#query-log-space").append('<div class="alert alert-info query-log"><button class="close" data-dismiss="alert"></button>' + query + '</div>')
 }
 
+var GraphData = function() {
+	this.dates = [];
+	this.map_brand = {};
+	this.opening_prices = {};
+	this.words = {};
+	return {
+		init : function() {
+			this.dates = [];
+			this.map_brand = {};
+			this.opening_prices = {};
+			this.words = {};
+		},
+		addStockpriceData : function(response) {
+			map_brand[selected_brand_name] = selected_brand_code;
+			array = [];
+			for(i=0; i<response.data.length; i++){
+				this.dates.push(response.data[i].date);
+				array.push(Number(response.data[i].opening_price));
+			}
+			this.opening_prices[selected_brand_name] = array;
+			console.log(response);
+		},
+		getStockPrice : function () {
+			if (selected_brand_name in this.map_brand != true){
+				this.map_brand[selected_brand_name] = selected_brand_code;
+			}
+			console.log(this.map_brand);
+			for (brand_name in this.map_brand){
+				baseURL = "http://www.ai.cs.kobe-u.ac.jp/~fujikawa/softwares/StockDemo/api/getStockPrice?";
+				var data = {
+					brand_code : this.map_brand[brand_name],
+					from : $("#input_date").attr("value"),
+					to: computeToDate()
+				};
+				console.log(data);
+				$.ajax({
+					type: "GET",
+					url: baseURL,
+					data: data,
+					dataType: 'jsonp',
+					success: function(response){
+						console.log(response);
+						addQueryLog(selected_brand_name);
+						GraphData.init();
+						GraphData.addStockpriceData(response);
+						GraphData.drawChart();
+					}
+				});
+			}
+		},
+		getMapBrand : function () {
+			console.log(this.map_brand);
+			return this.map_brand;
+		},
+		drawChart : function () {
+			console.log("test");
+			data = new google.visualization.DataTable();
+			data.addColumn('string', '日付');
+			array = [];
+			for (brand_name in this.map_brand){
+				data.addColumn('number', brand_name);
+			}
+
+			for (i = 0; i < this.dates.length; i++){
+				tmp = [];
+				tmp.push(this.dates[i]);
+				for (brand_name in this.map_brand){
+
+					console.log(this.opening_prices[brand_name]);
+					tmp.push(this.opening_prices[brand_name][i]);
+				}
+				array.push(tmp);
+			}
+			console.log(array);
+			data.addRows(array);
+
+			google.setOnLoadCallback(drawChart(data));
+		}
+	};
+}();
+
 var Search = function () {
-    return {
-        //main function to initiate the module
-        init: function () {
-            if (jQuery().datepicker) {
-                $('.date-picker').datepicker({format: 'yyyy-mm-dd'});
-            }
-            App.initFancybox();
-        }
-    };
+	return {
+		//main function to initiate the module
+		init: function () {
+			if (jQuery().datepicker) {
+				$('.date-picker').datepicker({
+					format: 'yyyy-mm-dd'
+				});
+			}
+			App.initFancybox();
+		}
+	};
 }();
 
 function drawChart(data) {
