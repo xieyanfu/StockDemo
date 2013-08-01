2cd1578bb06e360880938c3fff2213d5b712b327
Kazuki Fujikawa <k.fujikawa1014@gmail.com>
2013-08-01 17:10:52 +0900

    stockprice showed

--- js/nikkei.js
+++ js/nikkei.js
@@ -2,6 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+function toLowerCase(str){
+	return str.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+		return String.fromCharCode(s.charCodeAt(0) - 65248);
+	});
+}
 
 var KijiData = function() {
 	this.page_type = "";
@@ -31,6 +36,7 @@ var KijiData = function() {
 					start : this.page_num * 10
 				};
 			}
+			this.blockUI($("#kiji_window"));
 			$.ajax({
 				type: "GET",
 				url: baseURL,
@@ -44,7 +50,7 @@ var KijiData = function() {
 					}
 
 					KijiData.drawKijis();
-					App.unblockUI($("#chart_window"));
+					KijiData.unblockUI($("#kiji_window"));
 				}
 			});
 		},
@@ -56,21 +62,37 @@ var KijiData = function() {
 			console.log(this.kijis);
 			for (i = 0; i < this.kijis.data.length; i++){
 				html = "<li class='kiji span12'><div class='kiji_text span8'>";
-				html += "<h4>" + this.kijis.data[i].kiji_midashi + "</h3>";
-				html += "<p>" + this.kijis.data[i].kiji_headline + "</p></div>";
+				console.log(this.kijis.synonyms);
+				for (n = 0; n < this.kijis.synonyms.length; n++){
+					console.log(this.kijis.synonyms[n]);
+					this.kijis.data[i].kiji.kiji_headline = toLowerCase(this.kijis.data[i].kiji.kiji_headline).split(this.kijis.synonyms[n]).join("<strong>" + this.kijis.synonyms[n] + "</strong>");
+				}
+				html += "<div class='kiji_midashi'><h4>" + this.kijis.data[i].kiji.kiji_midashi + "</h4>";
+				html += "<h5>[ " + this.kijis.data[i].kiji.date + " ]</h5></div>";
+				html += "<p>" + this.kijis.data[i].kiji.kiji_headline + "</p></div>";
 				html += "<ul class='span4 kiji_entity'>";
 				if (this.page_type == "view"){
-//					html += '<li class="kiji_rate_' + this.kijis.data[i].rate + '"><span>rate: '+ this.kijis.data[i].rate +'</span></li>';
 					html += '<li><ul class="unstyled inline star">';
-					for (n = 0; n < this.kijis.data[i].rate; n++){
+					for (n = 0; n < this.kijis.data[i].kiji.rate; n++){
 						html += '<li><i class="icon-star"></i></li>';
 					}
-					for (n = 0; n < 3 - this.kijis.data[i].rate; n++){
+					for (n = 0; n < 3 - this.kijis.data[i].kiji.rate; n++){
 						html += '<li><i class="icon-star-empty"></i></li>';
 					}
 					html += '</ul></li>';
 				}
-				html += '<li class="kiji_date"><span>date: '+ this.kijis.data[i].date +'</span></li>';
+				for (n = 0; n < this.kijis.data[i].stockprice.length; n++){
+					diff = 0;
+					if (this.kijis.data[i].stockprice[n].opening_price)
+						diff = Math.round(((this.kijis.data[i].stockprice[n].closing_price - this.kijis.data[i].stockprice[n].opening_price) / this.kijis.data[i].stockprice[n].opening_price) * 100);
+					console.log(diff);
+					pos_neg = "";
+					if (diff > 0)
+						pos_neg = "_p";
+					else if (diff < 0)
+						pos_neg = "_n";
+					html += '<li class="kiji_stockprice' + pos_neg + '"><p>'+ this.kijis.data[i].stockprice[n].company_name +'</p><span>' + diff +  '% ('+ this.kijis.data[i].stockprice[n].date +  ')</span></li>';
+				}
 				html += "</ul>";
 				html += "</li>";
 
@@ -95,7 +117,7 @@ var KijiData = function() {
 			baseURL = "http://www.ai.cs.kobe-u.ac.jp/~fujikawa/softwares/StockDemo/api/annotateKiji?";
 			data = {
 				rate : rate,
-				kiji_id : this.kijis.data[0].kiji_id
+				kiji_id : this.kijis.data[0].kiji.kiji_id
 			};
 			$.ajax({
 				type: "GET",
@@ -106,7 +128,33 @@ var KijiData = function() {
 					KijiData.getKiji();
 				}
 			});
-		}
+		},
+		blockUI: function (el, centerY) {
+            jQuery(el).block({
+                message: '<img src="../assets/img/ajax-loading.gif" align="">',
+                centerY: centerY != undefined ? centerY : true,
+                css: {
+                    top: '10%',
+                    border: 'none',
+                    padding: '2px',
+                    backgroundColor: 'none'
+                },
+                overlayCSS: {
+                    backgroundColor: '#000',
+                    opacity: 0.05,
+                    cursor: 'wait'
+                }
+            });
+        },
+
+        // wrapper function to  un-block element(finish loading)
+        unblockUI: function (el) {
+            jQuery(el).unblock({
+                onUnblock: function () {
+                    jQuery(el).removeAttr("style");
+                }
+            });
+        },
 
 	};
 }();
